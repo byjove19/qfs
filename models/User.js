ef9519a6fd65b60b988dd2b690b10c96132fe50c
byjove19 <odionyejovy@gmail.com>
2025-10-08 11:59:28 +0100

    Profile functionality

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -74,14 +73,11 @@ currency: {
   timestamps: true
 });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
-});
 
 userSchema.methods.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+   return this.password === candidatePassword;
 };
+
+
 
 module.exports = mongoose.model('User', userSchema);
